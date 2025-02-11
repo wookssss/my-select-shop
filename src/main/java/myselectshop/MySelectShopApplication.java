3ac9134598cea4c66f9b3a825d1faa252867c6e5
Jeongwook Woo <dnwjddnr115@naver.com>
2025-02-11 15:24:54 +0900

    Controller 테스트

--- src/main/java/myselectshop/MySelectShopApplication.java
+++ src/main/java/myselectshop/MySelectShopApplication.java
@@ -2,11 +2,9 @@ package myselectshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableJpaAuditing
 @SpringBootApplication
 public class MySelectShopApplication {
     public static void main(String[] args) {
