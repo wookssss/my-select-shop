bdff4521d5a0a5676188f10cf8b805da1e4c5d14
Jeongwook Woo <dnwjddnr115@naver.com>
2025-02-11 15:51:58 +0900

    Top5 회원 찾기 구현

--- src/main/java/myselectshop/controller/ProductController.java
+++ src/main/java/myselectshop/controller/ProductController.java
@@ -4,14 +4,14 @@ import lombok.RequiredArgsConstructor;
 import myselectshop.dto.ProductMypriceRequestDto;
 import myselectshop.dto.ProductRequestDto;
 import myselectshop.dto.ProductResponseDto;
+import myselectshop.entity.ApiUseTime;
+import myselectshop.entity.User;
+import myselectshop.repository.ApiUseTimeRepository;
 import myselectshop.security.UserDetailsImpl;
 import myselectshop.service.ProductService;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +19,39 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    private final ApiUseTimeRepository apiUseTimeRepository;
 
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return productService.createProduct(requestDto, userDetails.getUser());
+        // 측정 시작 시간
+        long startTime = System.currentTimeMillis();
+
+        try {
+            // 응답 보내기
+            return productService.createProduct(requestDto, userDetails.getUser());
+        } finally {
+            // 측정 종료 시간
+            long endTime = System.currentTimeMillis();
+            // 수행시간 = 종료 시간 - 시작 시간
+            long runTime = endTime - startTime;
+
+            // 로그인 회원 정보
+            User loginUser = userDetails.getUser();
+
+            // API 사용시간 및 DB 에 기록
+            ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser)
+                    .orElse(null);
+            if (apiUseTime == null) {
+                // 로그인 회원의 기록이 없으면
+                apiUseTime = new ApiUseTime(loginUser, runTime);
+            } else {
+                // 로그인 회원의 기록이 이미 있으면
+                apiUseTime.addUseTime(runTime);
+            }
+
+            System.out.println("[API Use Time] Username: " + loginUser.getUsername() + ", Total Time: " + apiUseTime.getTotalTime() + " ms");
+            apiUseTimeRepository.save(apiUseTime);
+        }
     }
 
     @PutMapping("/products/{id}")
---
+++ src/main/java/myselectshop/repository/ApiUseTimeRepository.java
@@ -0,0 +1,11 @@
+package myselectshop.repository;
+
+import myselectshop.entity.ApiUseTime;
+import myselectshop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ApiUseTimeRepository extends JpaRepository<ApiUseTime, Long> {
+    Optional<ApiUseTime> findByUser(User user);
+}
