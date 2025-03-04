23dd700bf984920a32bc8ce8ca1e0a675328ad5e
Jeongwook Woo <dnwjddnr115@naver.com>
2025-03-04 12:50:45 +0900

    API 예외처리

--- src/main/java/myselectshop/controller/FolderController.java
+++ src/main/java/myselectshop/controller/FolderController.java
@@ -3,8 +3,11 @@ package myselectshop.controller;
 import lombok.RequiredArgsConstructor;
 import myselectshop.dto.FolderRequestDto;
 import myselectshop.dto.FolderResponseDto;
+import myselectshop.exception.RestApiException;
 import myselectshop.security.UserDetailsImpl;
 import myselectshop.service.FolderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,5 +29,16 @@ public class FolderController {
     @GetMapping("/folders")
     public List<FolderResponseDto> getFolders(@AuthenticationPrincipal UserDetailsImpl userDetails){
         return folderService.getFolders(userDetails.getUser());
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(
+            // HTTP body
+            restApiException,
+            // HTTP status code
+            HttpStatus.BAD_REQUEST
+        );
     }
 }
